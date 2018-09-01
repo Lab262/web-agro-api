d4733b132b40f639f55f3201a3ec8647eba4802d
luisresende <luisfrvilela@gmail.com>
2018-09-01 20:54:44 -0300

    Create updateMeasuredSupply endpoint

--- cloud/supply-management.js
+++ cloud/supply-management.js
@@ -24,16 +24,26 @@ Parse.Cloud.afterSave("PurchaseTransaction", function (request, response) {
 @measuredSupply: number
 */
 Parse.Cloud.define("updateMeasuredSupply", function (request, response) {
+    updateSupplyStatistics(request.params.productId, request.params.cooperativeId, request.params.measuredSupply).then(result => {
+        response.success(result)
+    }).catch(err => {
+        response.error(err)
+    });
+});
+
+function updateSupplyStatistics(productId, cooperativeId, measuredSupply) {
+
+    var promise = new Parse.Promise();
 
     var transactionDate = Moment(new Date()).add(1, 'day');
     var startDate = Moment(transactionDate).startOf('day').toDate()
     var endDate = Moment(transactionDate).endOf('day').toDate()
 
     var product = new Parse.Object('Product');
-    product.id = request.params.productId;
+    product.id = productId;
     var cooperative = new Parse.Object('Cooperative');
-    cooperative.id = request.params.cooperativeId;
-    var measuredSupply = parseFloat(request.params.measuredSupply)
+    cooperative.id = cooperativeId;
+    var measuredSupply = measuredSupply ? parseFloat(measuredSupply) : undefined
     var query = new Parse.Query('SupplyStatistics')
     query.equalTo('product', product);
     query.equalTo('cooperative', cooperative);
@@ -41,19 +51,45 @@ Parse.Cloud.define("updateMeasuredSupply", function (request, response) {
     query.first().then(result => {
         var supplyStatistic = result;
         if (supplyStatistic && Moment(supplyStatistic.get('createdAt')).isSame(transactionDate, 'day')/*alreadyExistSupplyStatisticsToday*/) {
-            supplyStatistic.set('measuredSupply', measuredSupply);
-            var supplyWaste = supplyStatistic.get('measuredSupply') - supplyStatistic.get('estimatedSupply');
-            supplyStatistic.set('supplyWaste', supplyWaste);
-            supplyStatistic.save().then(result => {
-                response.success(result);
-            }).catch(err => {
-                response.error(err)
-            })
+            if (measuredSupply) {
+                supplyStatistic.set('measuredSupply', measuredSupply);
+                var supplyWaste = supplyStatistic.get('measuredSupply') - supplyStatistic.get('estimatedSupply');
+                supplyStatistic.set('supplyWaste', supplyWaste);
+                supplyStatistic.save().then(result => {
+                    promise.resolve(result);
+                }).catch(err => {
+                    promise.reject(err)
+                })
+            } else {
+                getTransactionAmountByProduct("SalesTransaction", cooperative, product, startDate, endDate).then(salesAmount => {
+                    newSupplyStatistics.set('totalSales', salesAmount)
+                    return getTransactionAmountByProduct("PurchaseTransaction", cooperative, product, startDate, endDate)
+                }).then(purchaseAmount => {
+                    newSupplyStatistics.set('totalPurchase', purchaseAmount)
+                    var salesPurchasesDifference = newSupplyStatistics.get('totalPurchase') - newSupplyStatistics.get('totalSales')
+                    newSupplyStatistics.set('salesPurchasesDifference', salesPurchasesDifference)
+
+                    if (supplyStatistic) {
+                        var estimatedSupply = salesPurchasesDifference + supplyStatistic.get('measuredSupply'); //sales diff of today + measured of last day
+                        newSupplyStatistics.set('estimatedSupply', estimatedSupply);
+                    } else {
+                        newSupplyStatistics.set('estimatedSupply', salesPurchasesDifference);
+                    }
+                    var supplyWaste = newSupplyStatistics.get('measuredSupply') - newSupplyStatistics.get('estimatedSupply');
+                    newSupplyStatistics.set('supplyWaste', supplyWaste);
+                    return newSupplyStatistics.save()
+                }).then(result => {
+                    promise.resolve(result);
+                }).catch(err => {
+                    promise.reject(err)
+                })
+            }
+
         } else {
             var newSupplyStatistics = new Parse.Object('SupplyStatistics');
             newSupplyStatistics.set('product', product);
             newSupplyStatistics.set('cooperative', cooperative);
-            newSupplyStatistics.set('measuredSupply', measuredSupply);
+            
             getTransactionAmountByProduct("SalesTransaction", cooperative, product, startDate, endDate).then(salesAmount => {
                 newSupplyStatistics.set('totalSales', salesAmount)
                 return getTransactionAmountByProduct("PurchaseTransaction", cooperative, product, startDate, endDate)
@@ -68,19 +104,25 @@ Parse.Cloud.define("updateMeasuredSupply", function (request, response) {
                 } else {
                     newSupplyStatistics.set('estimatedSupply', salesPurchasesDifference);
                 }
+                if (measuredSupply) {
+                    newSupplyStatistics.set('measuredSupply', measuredSupply);
+                } else {
+                    newSupplyStatistics.set('measuredSupply', newSupplyStatistics.get('estimatedSupply'));
+                }
                 var supplyWaste = newSupplyStatistics.get('measuredSupply') - newSupplyStatistics.get('estimatedSupply');
                 newSupplyStatistics.set('supplyWaste', supplyWaste);
                 return newSupplyStatistics.save()
             }).then(result => {
-                response.success(result);
+                promise.resolve(result);
             }).catch(err => {
-                response.error(err)
+                promise.reject(err)
             })
         }
     }).catch(err => {
-        response.error(err)
+        promise.reject(err)
     })
-});
+    return promise;
+}
 
 function getTransactionAmountByProduct(transactionClass, cooperative, product, startDate, endDate) {
     var promise = new Parse.Promise();
